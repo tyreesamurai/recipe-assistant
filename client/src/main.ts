a813730380cbb07d3321abb0c3dbe3b1cbced23c
Tyree Carpenter <carpentertd@vcu.edu>
2025-09-01 09:58:24 -0400

    Just a commit for when I start this project again

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
+import ToastService from 'primevue/toastservice'
 import router from './router'
 
 const app = createApp(App)
@@ -13,6 +14,7 @@ app.use(PrimeVue, {
   ripple: true,
 })
 
+app.use(ToastService)
 app.use(router)
 
 app.mount('#app')
